da4bd4281b79d6b98f4711d1d1aa2cabae72d8e3
Justin Smits <jnsmits@student.che.nl>
2022-09-03 23:34:05 +0200

    Inlogpagina nu met eigen inloggegevens werkend.

--- login.js
+++ login.js
@@ -1,42 +1,17 @@
-var jwt = localStorage.getItem("jwt");
-if (jwt != null) {
-  window.location.href = './index.html'
-}
-
 function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "https://www.mecallapi.com/api/login");
-  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhttp.send(JSON.stringify({
-    "username": username,
-    "password": password
-  }));
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4) {
-      const objects = JSON.parse(this.responseText);
-      console.log(objects);
-      if (objects['status'] == 'ok') {
-        localStorage.setItem("jwt", objects['accessToken']);
-        Swal.fire({
-          text: objects['message'],
-          icon: 'success',
-          confirmButtonText: 'OK'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.href = './inloggen.html';
-          }
-        });
-      } else {
-        Swal.fire({
-          text: objects['message'],
-          icon: 'error',
-          confirmButtonText: 'OK'
-        });
-      }
+  if ( username == "admin" && password == "admin"){
+    alert ("Login successfully");
+    window.location = "index.html"; // Redirecting to other page.
+    return true;
+  }
+  else if(username != true)
+    alert("Wrong password!");
+    // Disabling fields after 3 attempts.
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("submit").disabled = true;
+    return false;
     }
-  };
-  return false;
-}
